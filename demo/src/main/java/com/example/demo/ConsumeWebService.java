e0f1e4a0fd617a1a0a80f48808484950fc57ed02
Nizar Mota <motta.reig1@hotmail.com>
2021-03-01 01:08:49 -0300

    Criando Classe para consumir API

---
+++ demo/src/main/java/com/example/demo/ConsumeWebService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class ConsumeWebService {
+   @Autowired
+   RestTemplate restTemplate;
+
+   @RequestMapping(value = "/template/products")
+   public String getProductList() {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      HttpEntity <String> entity = new HttpEntity<String>(headers);
+      
+      return restTemplate.exchange("
+         http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();
+   }
+}
