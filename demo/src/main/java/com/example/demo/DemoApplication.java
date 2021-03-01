e745745c447a27cf0302fd4d215a7c4aa3816af4
Nizar Mota <motta.reig1@hotmail.com>
2021-03-01 11:56:46 -0300

    Teste

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class DemoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
